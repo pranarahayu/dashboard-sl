c28c64476c9c7b218fb7d941bf579d07c188a109
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2026-01-12 16:33:11 +0700

    Update 4_Dashboard.py

--- pages/4_Dashboard.py
+++ pages/4_Dashboard.py
@@ -34,23 +34,22 @@ dfx = pd.merge(dfx, db, on='Player ID', how='left')
 
 mlist = get_list(df)
 
-try:
-    comps, teams, players = st.tabs(['Competitions', 'Team Stats', 'Player Stats'])
+comps, teams, players = st.tabs(['Competitions', 'Team Stats', 'Player Stats'])
 
-    with comps:
-      st.write('Hai')
-    with teams:
-      st.write('Hai')
-    with players:
-      col1, col2, col3, col4, col5 = st.columns(5)
-      with col1:
+with comps:
+    st.write('Hai')
+with teams:
+    st.write('Hai')
+with players:
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
         season = st.selectbox('Select Season', ['2025-26'], key='1')
         data = dfx.copy()
         team = st.multiselect('Select Teams', pd.unique(data['Team']), key='2')
         all_teams = st.checkbox('Select All Teams', key='3')
         if all_teams:
           team = pd.unique(data['Team'])
-      with col2:
+    with col2:
         temp = data[data['Team'].isin(team)]
         month = st.multiselect('Select Months', pd.unique(temp['Month']), key='4')
         all_months = st.checkbox('Select All Months', key='5')
@@ -61,12 +60,12 @@ try:
         all_gws = st.checkbox('Select All Gameweeks', key='7')
         if all_gws:
           gws = pd.unique(temp['Gameweek'])
-      with col3:
+    with col3:
         temp = temp[temp['Gameweek'].isin(gws)]
         venue = st.multiselect('Select Venues', pd.unique(temp['Home/Away']), key='8')
         temp = temp[temp['Home/Away'].isin(venue)]
         nats = st.multiselect('Select Nationalities', pd.unique(temp['Nat. Status']), key='9')
-      with col4:
+    with col4:
         temp = temp[temp['Nat. Status'].isin(nats)]
         age_group = st.multiselect('Select Age Groups', pd.unique(temp['Age Group']), key='10')
         temp = temp[temp['Age Group'].isin(age_group)]
@@ -74,21 +73,19 @@ try:
         all_pos = st.checkbox('Select All Positions', key='12')
         if all_pos:
           positions = pd.unique(temp['Position'])
-      with col5:
+    with col5:
         temp = temp[temp['Position'].isin(positions)]
         mins = st.number_input('Input minimum mins. played', min_value=0, max_value=3060, step=90, key=13)
         metrik = st.multiselect('Select Metrics', mlist, key='14')
-      cat = st.selectbox('Select Category', ['Total', 'per 90'], key='15')
-      show_player_data = data_player(dfx, db, team, month, gws, venue,
-                                     age_group, nats, positions, mins,
-                                     metrik, cat)
+    cat = st.selectbox('Select Category', ['Total', 'per 90'], key='15')
+    show_player_data = data_player(dfx, db, team, month, gws, venue,
+                                   age_group, nats, positions, mins,
+                                   metrik, cat)
 
-      buffer = io.BytesIO()
-      with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+    buffer = io.BytesIO()
+    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
         show_player_data.to_excel(writer, sheet_name='Sheet1', index=False)
-      download = st.download_button(label="Download data as Excel", data=buffer.getvalue(),
-                                    file_name='player-data_downloaded ('+date+').xlsx',
-                                    mime='application/vnd.ms-excel', key = 16)
-      st.dataframe(show_player_data.drop(['Player ID'], axis=1), hide_index=True)
-except:
-    st.dataframe(db, hide_index=True)
+    download = st.download_button(label="Download data as Excel", data=buffer.getvalue(),
+                                  file_name='player-data_downloaded ('+date+').xlsx',
+                                  mime='application/vnd.ms-excel', key = 16)
+    st.dataframe(show_player_data.drop(['Player ID'], axis=1), hide_index=True)
